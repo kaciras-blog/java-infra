6d56778e512b3d1e3f311e3eff55ebdfe6235b3b
Kaciras <kaciras@outlook.com>
2018-08-08 15:31:36 +0800

    add codec autoconfigure support.

---
+++ src/main/java/net/kaciras/blog/infrastructure/codec/KxCodecConfiguration.java
@@ -0,0 +1,28 @@
+package net.kaciras.blog.infrastructure.codec;
+
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KxCodecConfiguration {
+
+	@ConditionalOnClass(ConfigurationCustomizer.class)
+	@Bean
+	public ConfigurationCustomizer mybatisCustomizer() {
+		return config -> {
+			var registry = config.getTypeHandlerRegistry();
+			registry.register(ImageRefrenceTypeHandler.class);
+			registry.register(IpAddressTypeHandler.class);
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@ConditionalOnClass(Jackson2ObjectMapperBuilderCustomizer.class)
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+		return builder -> builder.modulesToInstall(ExtendsCodecModule.class);
+	}
+}
