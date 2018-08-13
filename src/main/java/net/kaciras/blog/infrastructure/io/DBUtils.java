17de502c6076172c03dd6032a7a05881d53e604c
Kaciras <kaciras@outlook.com>
2018-08-13 09:58:34 +0800

    add DBUtils to verify dao operation.

---
+++ src/main/java/net/kaciras/blog/infrastructure/io/DBUtils.java
@@ -0,0 +1,30 @@
+package net.kaciras.blog.infrastructure.io;
+
+import net.kaciras.blog.infrastructure.exception.ResourceNotFoundException;
+
+public final class DBUtils {
+
+	/**
+	 * 用于检查Update，Delete等SQL语句是否产生了影响，没产生影响时将抛出异常
+	 *
+	 * @param rows 影响行数
+	 * @throws ResourceNotFoundException 如果没有影响任何行
+	 */
+	public static void checkEffective(int rows) {
+		if (rows <= 0) throw new ResourceNotFoundException();
+	}
+
+	public static <T> T checkNotNullResource(T obj) {
+		if (obj == null)
+			throw new ResourceNotFoundException();
+		return obj;
+	}
+
+	public static <T> T checkNotNullResource(T obj, String message) {
+		if (obj == null)
+			throw new ResourceNotFoundException(message);
+		return obj;
+	}
+
+	private DBUtils() {}
+}
