34af5b049c656d89dd662c4f1bacfc096aa38b47
Kaciras <Kaciras@outlook.com>
2019-06-20 11:45:52 +0800

    add BitSet benchmark to HexMatchPerf

--- src/test/java/net/kaciras/blog/infrastructure/codec/HexMatchPerf.java
+++ src/test/java/net/kaciras/blog/infrastructure/codec/HexMatchPerf.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.BitSet;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
@@ -15,12 +16,19 @@ import java.util.regex.Pattern;
 @State(Scope.Thread)
 @Fork(1)
 @Measurement(iterations = 5, time = 5)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class HexMatchPerf {
 
 	private static final Pattern REGEX = Pattern.compile("^[0-9a-fA-F]{64}$");
+	private static final BitSet BIT_SET = new BitSet();
 
-	@Param({"f1894c00ba-default-background", "0de735be2d228599d4a48fe37f7cdc45b6134296a9bd59959590f7cefffeaf96"})
+	static {
+		for (char c = '0'; c <= '9'; c++) BIT_SET.set(c);
+		for (char c = 'a'; c <= 'z'; c++) BIT_SET.set(c);
+		for (char c = 'A'; c <= 'Z'; c++) BIT_SET.set(c);
+	}
+
+	@Param({"f1894c00ba-default-非英文字符", "0de735be2d228599d4a48fe37f7cdc45b6134296a9bd59959590f7cefffeaf96"})
 	private String text;
 
 	private boolean doBySwitch(char character) {
@@ -55,35 +63,34 @@ public class HexMatchPerf {
 
 	@Benchmark
 	public void ifRange(Blackhole blackhole) {
-		for (int i = 0; i < 100; i++) {
-			for (var ch : text.toCharArray()) {
-				blackhole.consume(CodecUtils.isHexDigit(ch));
-			}
+		for (var c: text.toCharArray()) {
+			blackhole.consume(CodecUtils.isHexDigit(c));
 		}
 	}
 
 	@Benchmark
 	public void bySwitch(Blackhole blackhole) {
-		for (int i = 0; i < 100; i++) {
-			for (var ch : text.toCharArray()) {
-				blackhole.consume(doBySwitch(ch));
-			}
+		for (var c : text.toCharArray()) {
+			blackhole.consume(doBySwitch(c));
 		}
 	}
 
 	@Benchmark
 	public void regexp(Blackhole blackhole) {
-		for (int i = 0; i < 100; i++) {
-			blackhole.consume(REGEX.matcher(text).find());
-		}
+		blackhole.consume(REGEX.matcher(text).find());
 	}
 
 	@Benchmark
 	public void clr(Blackhole blackhole) {
-		for (int i = 0; i < 100; i++) {
-			for (var ch : text.toCharArray()) {
-				blackhole.consume(Character.digit(ch, 16) != -1);
-			}
+		for (var c : text.toCharArray()) {
+			blackhole.consume(Character.digit(c, 16) != -1);
+		}
+	}
+
+	@Benchmark
+	public void bitSet(Blackhole blackhole) {
+		for (var c : text.toCharArray()) {
+			blackhole.consume(BIT_SET.get(c));
 		}
 	}
 
