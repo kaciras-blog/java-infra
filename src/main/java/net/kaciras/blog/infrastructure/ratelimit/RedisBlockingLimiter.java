821d84c56e42b1df988453bd0678947f621528f7
Kaciras <Kaciras@outlook.com>
2019-06-24 18:49:34 +0800

    ratelimit: change RedisBlockingLimiter to support multilevel block time

--- src/main/java/net/kaciras/blog/infrastructure/ratelimit/RedisBlockingLimiter.java
+++ src/main/java/net/kaciras/blog/infrastructure/ratelimit/RedisBlockingLimiter.java
@@ -1,22 +1,50 @@
 package net.kaciras.blog.infrastructure.ratelimit;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.time.Clock;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
-@Setter
 public final class RedisBlockingLimiter implements RateLimiter {
 
 	private final String namespace;
 	private final RateLimiter inner;
-	private final RedisTemplate<String, Object> redis;
+	private final RedisConnectionFactory redisFactory;
+	private final Clock clock;
 
-	private Duration banTime = Duration.ofHours(1);
+	private List<Duration> blockTimes = Collections.emptyList();
+
+	@Setter
 	private boolean refreshOnReject;
+
+	/**
+	 * 设置封禁时间列表，列表中从前到后的等级逐渐升高，后面的时间必须大于前面的，所有时间都不能为负。
+	 *
+	 * @param blockTimes 封禁时间列表
+	 * @throws IllegalArgumentException 如果参数不满足上述要求
+	 */
+	public void setBlockTimes(@NonNull List<Duration> blockTimes) {
+		var max = Duration.ZERO;
+		for (var time : blockTimes) {
+			if (time.compareTo(max) < 0) {
+				throw new IllegalArgumentException("封禁时间存在负数，或小于前面的");
+			}
+			max = time;
+		}
+		this.blockTimes = blockTimes;
+	}
 
 	/*
 	 * 【更新】之前版本使用了异步化机制，将对内层限流器的调用和设置封禁记录这两操作放在其他线程中，
@@ -25,24 +53,83 @@ public final class RedisBlockingLimiter implements RateLimiter {
 	 */
 	@Override
 	public long acquire(@NonNull String id, int permits) {
-		var blockKey = namespace + id;
 
-		var timeToLive = redis.getExpire(blockKey);
-		if (timeToLive == null) {
-			throw new IllegalStateException("限流操作不支持Redis管道和事务");
+		// Simply delegate to inner when no blockTimes added.
+		if (blockTimes.isEmpty()) {
+			return inner.acquire(id, permits);
 		}
-		if (timeToLive > 0) {
-			if (refreshOnReject) {
-				redis.opsForValue().set(blockKey, "", banTime);
+
+		// Why RedisConnection not implements AutoClosable? Is it reminds user to call RedisConnectionUtils?
+		var connection = redisFactory.getConnection();
+		try {
+			return doAcquire(connection, id, permits);
+		} finally {
+			RedisConnectionUtils.releaseConnection(connection, redisFactory);
+		}
+	}
+
+	private long doAcquire(RedisConnection connection, String id, int permits) {
+		var now = (int) clock.instant().getEpochSecond();
+		var blockKey = (namespace + id).getBytes(StandardCharsets.UTF_8);
+		var record = deserialize(connection.get(blockKey));
+
+		if (record != null) {
+			var bTime = record.getBlockingTime();
+			var waitTime = record.beginTime + bTime - now;
+
+			if (waitTime > 0) {
+				if (refreshOnReject) {
+					record.beginTime = now;
+					connection.setEx(blockKey, bTime, record.serialize());
+				}
+				return waitTime;
 			}
-			return timeToLive;
 		}
 
 		var waitTime = inner.acquire(id, permits);
 		if (waitTime <= 0) {
 			return waitTime;
 		}
-		redis.opsForValue().set(blockKey, "", banTime);
-		return banTime.toSeconds();
+		if (record == null) {
+			record = new BlockingRecord(0, now);
+		} else {
+			record.increaseLevel(now);
+		}
+		connection.setEx(blockKey, record.getObservationPeriod(), record.serialize());
+		return record.getBlockingTime();
+	}
+
+	private BlockingRecord deserialize(@Nullable byte[] data) {
+		if (data == null) {
+			return null;
+		}
+		var buffer = ByteBuffer.wrap(data);
+		return new BlockingRecord(buffer.getInt(), buffer.getInt());
+	}
+
+	// 仍然用的是32位秒数，最大2038年，本代码肯定用不到那么久
+	@AllArgsConstructor
+	private final class BlockingRecord {
+
+		private int level;
+		private int beginTime;
+
+		private void increaseLevel(int now) {
+			level = Math.min(level + 1, blockTimes.size() - 1);
+			beginTime = now;
+		}
+
+		private long getBlockingTime() {
+			return blockTimes.get(level).toSeconds();
+		}
+
+		private long getObservationPeriod() {
+			var index = Math.min(level + 1, blockTimes.size() - 1);
+			return blockTimes.get(index).toSeconds();
+		}
+
+		private byte[] serialize() {
+			return ByteBuffer.allocate(8).putInt(level).putInt(beginTime).array();
+		}
 	}
 }
