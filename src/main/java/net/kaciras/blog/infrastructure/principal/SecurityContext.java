64e3f6ec64ce706b93d10d6c67c7e1a578cbeb43
Kaciras <Kaciras@outlook.com>
2019-06-27 22:03:12 +0800

    SecurityContext: remove unused methods

--- src/main/java/net/kaciras/blog/infrastructure/principal/SecurityContext.java
+++ src/main/java/net/kaciras/blog/infrastructure/principal/SecurityContext.java
@@ -4,6 +4,10 @@ import net.kaciras.blog.infrastructure.exception.PermissionException;
 
 import java.util.Objects;
 
+/**
+ * 把用户身份保存在线程本地变量里，方便随时获取，跟 Spring Security 里差不多的东西。
+ * 除此之外，还带有一些便捷方法。
+ */
 public final class SecurityContext {
 
 	private static final ThreadLocal<WebPrincipal> threadLocal = new ThreadLocal<>();
@@ -27,7 +31,7 @@ public final class SecurityContext {
 	}
 
 	/**
-	 * 检查当前的用户是否不是参数id所指定的用户。
+	 * 检查当前的用户是否不是参数 id 所指定的用户。
 	 * 因为一般当用户和所需用户不同时才需要额外处理，所以设计为反义方法。
 	 *
 	 * @param id 用户id
@@ -43,14 +47,5 @@ public final class SecurityContext {
 
 	public static void requireLogin() {
 		if (getPrincipal().isAnonymous()) throw new PermissionException();
-	}
-
-	public static boolean checkSelf(int id, String perm) {
-		var principal = getPrincipal();
-		return principal.getId() == id || principal.hasPermission(perm);
-	}
-
-	public static void requireSelf(int id, String perm) {
-		if (!checkSelf(id, perm)) throw new PermissionException();
 	}
 }
